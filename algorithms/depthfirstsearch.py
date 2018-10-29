3dca9ab9df5c50752188fae7f6029bc6ab571b4b
Freddie Karlbom <freddiekarlbom@gmail.com>
2018-10-29 14:51:17 +0100

    WIP - working exploration of graph

--- algorithms/depthfirstsearch.py
+++ algorithms/depthfirstsearch.py
@@ -1,2 +1,29 @@
 class DepthFirstSearch:
-    pass
+
+    def __init__(self, edges):
+        self.edges = edges
+        self.vertices = list(set([x for sublist in edges for x in sublist]))
+        self.explored = []
+        self.current_label = len(self.vertices)
+        self.labels = {}
+
+    def dfs(self, vertix, reverse=False):
+        self.explored.append(vertix)
+        tail, head = 0, 1
+        if reverse:
+            tail, head = head, tail
+        children = [x[head] for x in self.edges if x[tail] == vertix]
+        for child in children:
+            if child not in self.explored:
+                self.dfs(child, reverse)
+
+        self.labels[vertix] = self.current_label
+        self.current_label -= 1
+
+    def dfs_loop(self):
+        # Mark nodes unexplored
+        # current_label = len(self.vertices)
+
+        for vertix in self.vertices:
+            if vertix not in self.explored:
+                self.dfs(vertix)
--- tests/test_depthfirstsearch.py
+++ tests/test_depthfirstsearch.py
@@ -1,29 +1,71 @@
 import unittest
-from helpers import stanford
 from algorithms.depthfirstsearch import DepthFirstSearch
+from helpers import stanford
 
 class TestDepthFirstSearch(unittest.TestCase):
-    # As the test data (SCC.txt) isn't included in the repo, these tests will
-    # fail unless the data file is downloaded separately
-
-    # @classmethod
-    # def setUpClass(self):
-    #     filename = 'SCC.txt'
-    #     self.list_ = stanford.read_graph_file(filename, ' ')
-
-    @unittest.skip
-    def test_can_read_data(self):
-        self.assertEqual(self.list_[0], [1, 1])
 
     def test_can_load_depth_first_search_class(self):
-        dfs = DepthFirstSearch()
+        dfs = DepthFirstSearch([])
         self.assertEqual(type(dfs), DepthFirstSearch)
 
     def test_can_do_simple_search(self):
-        # Construct simple graph
         # perform search to find sinks
+        edges = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]]
+        graph = DepthFirstSearch(edges)
+        graph.dfs(1)
+        self.assertEqual(graph.explored, [1, 2, 3, 4, 5, 6])
+
+        # multiple branches
+        edges = [[1, 2], [1, 3],
+                     [3, 4], [4, 5], [5, 6],
+                     [2, 7], [7, 8], [8, 9]]
+        graph = DepthFirstSearch(edges)
+        graph.dfs(1)
+        self.assertEqual(sorted(graph.explored), [1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    def test_can_recursively_search_to_explore_complete_graph(self):
+        edges = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]]
+        graph = DepthFirstSearch(edges)
+        graph.dfs_loop()
+        self.assertEqual(sorted(graph.explored), [1, 2, 3, 4, 5, 6])
+
+        # multiple branches
+        edges = [[1, 2], [1, 3],
+                     [3, 4], [4, 5], [5, 6],
+                     [2, 7], [7, 8], [8, 9]]
+        graph = DepthFirstSearch(edges)
+        graph.dfs_loop()
+        self.assertEqual(sorted(graph.explored), [1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    def test_can_reverse_search(self):
+        edges = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]]
+        graph = DepthFirstSearch(edges)
+        graph.dfs(6, reverse=True)
+        self.assertEqual(graph.explored, [6, 5, 4, 3, 2, 1])
+        # Call the graph object with a reverse flag to search backwards
+
+    def test_can_assign_labels(self):
+        edges = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]]
+        graph = DepthFirstSearch(edges)
+        graph.dfs(1)
+        #self.assertEqual(graph.explored, [1, 2, 3, 4, 5, 6])
+        self.assertEqual(graph.labels[6], 6)
+        self.assertEqual(graph.labels[1], 1)
+
+        # multiple branches
+        edges = [[1, 2], [1, 3],
+                     [3, 4], [4, 5], [5, 6],
+                     [2, 7], [7, 8], [8, 9], [9, 10]]
+        graph = DepthFirstSearch(edges)
+        graph.dfs(1)
+        self.assertEqual(graph.labels[1], 1)
+        self.assertEqual(graph.labels[10], 10)
+
+    def test_can_get_fully_connected_parts(self):
+        @pass
 
     @unittest.skip
-    def test_can_reverse_search(self):
-        # Call the graph object with a reverse flag to search backwards
-        #
+    def test_can_read_data(self):
+        filename = 'SCC.txt'
+        vertices = stanford.read_graph_file(filename, ' ')
+        self.assertEqual(vertices[0], [1, 1])
