e007b0e34fca9842d2a41b42e0092e155800b5c2
Freddie Karlbom <freddiekarlbom@gmail.com>
2018-10-29 16:55:01 +0100

    working algorithm, but failing due to Python max number of recursions

--- algorithms/depthfirstsearch.py
+++ algorithms/depthfirstsearch.py
@@ -1,13 +1,18 @@
+import pandas as pd
+
 class DepthFirstSearch:
+    _current_leader = None
+    _iteration = None
 
     def __init__(self, edges):
         self.edges = edges
         self.vertices = list(set([x for sublist in edges for x in sublist]))
-        self.explored = []
         self.current_label = len(self.vertices)
-        self.labels = {}
+        self.explored = []
+        self.finishing_order = {}
+        self.vertix_groups = {}
 
-    def dfs(self, vertix, reverse=False):
+    def _dfs(self, vertix, reverse=False):
         self.explored.append(vertix)
         tail, head = 0, 1
         if reverse:
@@ -15,15 +20,39 @@ class DepthFirstSearch:
         children = [x[head] for x in self.edges if x[tail] == vertix]
         for child in children:
             if child not in self.explored:
-                self.dfs(child, reverse)
+                self._dfs(child, reverse)
 
-        self.labels[vertix] = self.current_label
-        self.current_label -= 1
+        if self._iteration == 'first':
+            self.finishing_order[vertix] = self.current_label
+            self.current_label -= 1
+        elif self._iteration == 'second':
+            self.vertix_groups[vertix] = self._current_leader
 
-    def dfs_loop(self):
-        # Mark nodes unexplored
-        # current_label = len(self.vertices)
+    def first_pass(self):
+        self._iteration = 'first'
+        self.current_label = len(self.vertices)
+        vertices = self.vertices
 
-        for vertix in self.vertices:
+        self.explored = []
+        for vertix in vertices:
+            print(f'Continuing for vertix {vertix}')
+            print(f'Explored: {self.explored}')
             if vertix not in self.explored:
-                self.dfs(vertix)
+                self._dfs(vertix, True)
+
+    def second_pass(self):
+        print('Second pass')
+        self._iteration = 'second'
+        run_order = sorted(self.finishing_order.items(), key=lambda x: x[1])
+        vertices = [x[0] for x in run_order]
+
+        self.explored = []
+        for vertix in vertices:
+            print(f'Continuing for vertix {vertix}')
+            if vertix not in self.explored:
+                self._current_leader = vertix
+                self._dfs(vertix, False)
+
+    def get_scc_sizes(self):
+        scc = [x for x in self.vertix_groups.values()]
+        return pd.Series(scc).value_counts()
--- tests/test_depthfirstsearch.py
+++ tests/test_depthfirstsearch.py
@@ -12,7 +12,7 @@ class TestDepthFirstSearch(unittest.TestCase):
         # perform search to find sinks
         edges = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]]
         graph = DepthFirstSearch(edges)
-        graph.dfs(1)
+        graph._dfs(1)
         self.assertEqual(graph.explored, [1, 2, 3, 4, 5, 6])
 
         # multiple branches
@@ -20,13 +20,13 @@ class TestDepthFirstSearch(unittest.TestCase):
                      [3, 4], [4, 5], [5, 6],
                      [2, 7], [7, 8], [8, 9]]
         graph = DepthFirstSearch(edges)
-        graph.dfs(1)
+        graph._dfs(1)
         self.assertEqual(sorted(graph.explored), [1, 2, 3, 4, 5, 6, 7, 8, 9])
 
     def test_can_recursively_search_to_explore_complete_graph(self):
         edges = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]]
         graph = DepthFirstSearch(edges)
-        graph.dfs_loop()
+        graph.first_pass()
         self.assertEqual(sorted(graph.explored), [1, 2, 3, 4, 5, 6])
 
         # multiple branches
@@ -34,38 +34,71 @@ class TestDepthFirstSearch(unittest.TestCase):
                      [3, 4], [4, 5], [5, 6],
                      [2, 7], [7, 8], [8, 9]]
         graph = DepthFirstSearch(edges)
-        graph.dfs_loop()
+        graph.first_pass()
         self.assertEqual(sorted(graph.explored), [1, 2, 3, 4, 5, 6, 7, 8, 9])
 
     def test_can_reverse_search(self):
         edges = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]]
         graph = DepthFirstSearch(edges)
-        graph.dfs(6, reverse=True)
+        graph._dfs(6, reverse=True)
         self.assertEqual(graph.explored, [6, 5, 4, 3, 2, 1])
         # Call the graph object with a reverse flag to search backwards
 
     def test_can_assign_labels(self):
         edges = [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]]
         graph = DepthFirstSearch(edges)
-        graph.dfs(1)
+        graph._iteration = 'first'
+        graph._dfs(1)
         #self.assertEqual(graph.explored, [1, 2, 3, 4, 5, 6])
-        self.assertEqual(graph.labels[6], 6)
-        self.assertEqual(graph.labels[1], 1)
+        self.assertEqual(graph.finishing_order[6], 6)
+        self.assertEqual(graph.finishing_order[1], 1)
 
         # multiple branches
         edges = [[1, 2], [1, 3],
                      [3, 4], [4, 5], [5, 6],
                      [2, 7], [7, 8], [8, 9], [9, 10]]
         graph = DepthFirstSearch(edges)
-        graph.dfs(1)
-        self.assertEqual(graph.labels[1], 1)
-        self.assertEqual(graph.labels[10], 10)
+        graph._iteration = 'first'
+        graph._dfs(1)
+        self.assertEqual(graph.finishing_order[1], 1)
+        self.assertEqual(graph.finishing_order[10], 10)
 
-    def test_can_get_fully_connected_parts(self):
-        @pass
+    def test_can_run_second_pass_based_on_finishing_time(self):
+        edges = [[1, 2], [2, 4], [4, 3], [3, 1], [2, 5], [5, 6], [6, 7], [7, 5]]
+        graph = DepthFirstSearch(edges)
+        graph.first_pass()
+        graph.second_pass()
 
-    @unittest.skip
-    def test_can_read_data(self):
+        self.assertEqual(graph.vertix_groups, {7: 5, 6: 5, 5: 5, 3: 1,
+                                               4: 1, 2: 1, 1: 1})
+
+    def test_can_get_ordered_list_of_strongly_connected_component_sizes(self):
+        edges = [[1, 2], [2, 4], [4, 3], [3, 1], [2, 5], [5, 6], [6, 7], [7, 5]]
+        graph = DepthFirstSearch(edges)
+        graph.first_pass()
+        graph.second_pass()
+        scc = graph.get_scc_sizes()
+        self.assertEqual(scc.tolist(), [4, 3])
+
+#    @unittest.skip
+    def test_with_stanford_data(self):
+        import sys
+        sys.setrecursionlimit(5000)
+
         filename = 'SCC.txt'
-        vertices = stanford.read_graph_file(filename, ' ')
-        self.assertEqual(vertices[0], [1, 1])
+        edges = stanford.read_graph_file(filename, ' ')
+        graph = DepthFirstSearch(edges)
+
+        print(f'Num vertices: {graph.current_label}')
+
+        print('starting first pass')
+        graph.first_pass()
+
+        print('starting second pass')
+        graph.second_pass()
+
+        print('calculating scc sizes')
+        scc = graph.get_scc_sizes()
+        breakpoint()
+
+        print(scc)
